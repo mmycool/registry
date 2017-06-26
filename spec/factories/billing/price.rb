8bc2f9c165e69b57c328fb59d4fc0a0267050c7c
Artur Beljajev <artur.beljajev@internet.ee>
2017-06-26 03:17:00 +0300

    Use price #effect_time instead of #valid_from
    #538

--- spec/factories/billing/price.rb
+++ spec/factories/billing/price.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :price, class: Billing::Price do
     price Money.from_amount(1)
-    valid_from Time.zone.parse('05.07.2010')
+    effect_time Time.zone.parse('05.07.2010')
     duration '1 year'
     operation_category Billing::Price.operation_categories.first
     zone
