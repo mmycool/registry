14d430032c96485477acdef0f8a2d3b85e4914b5
Artur Beljajev <artur.beljajev@internet.ee>
2017-11-09 19:24:43 +0200

    Require InvoiceItem#description
    #509

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,7 @@
 class InvoiceItem < ActiveRecord::Base
   belongs_to :invoice
 
+  validates :description, presence: true
   validates :quantity, presence: true
 
   def item_sum_without_vat
--- test/models/invoice_item_test.rb
+++ test/models/invoice_item_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class InvoiceItemTest < ActiveSupport::TestCase
+  def test_requires_description
+    item = InvoiceItem.new(description: nil)
+    item.validate
+    assert item.errors.added?(:description, :blank)
+  end
+
   def test_requires_quantity
     item = InvoiceItem.new(quantity: nil)
     item.validate
