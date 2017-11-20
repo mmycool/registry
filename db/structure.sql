abe64dd23951ceebafca7283fa945737356f201f
Artur Beljajev <artur.beljajev@internet.ee>
2017-11-20 12:26:51 +0200

    Increase `registrars.vat_rate` size
    #509

--- db/structure.sql
+++ db/structure.sql
@@ -2312,7 +2312,7 @@ CREATE TABLE registrars (
     legacy_id integer,
     reference_no character varying,
     test_registrar boolean DEFAULT false,
-    vat_rate numeric(3,2)
+    vat_rate numeric(4,3)
 );
 
 
@@ -4838,4 +4838,6 @@ INSERT INTO schema_migrations (version) VALUES ('20171111191117');
 INSERT INTO schema_migrations (version) VALUES ('20171112230729');
 
 INSERT INTO schema_migrations (version) VALUES ('20171116114948');
+
+INSERT INTO schema_migrations (version) VALUES ('20171120093221');
 
