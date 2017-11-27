6975a6792f4862f92a057c41a7cb18057e3528af
Artur Beljajev <artur.beljajev@internet.ee>
2017-11-27 03:44:00 +0200

    Allow 2 digits after decimal point in invoice VAT rate
    #509

--- db/structure.sql
+++ db/structure.sql
@@ -1121,7 +1121,7 @@ CREATE TABLE invoices (
     currency character varying NOT NULL,
     description character varying,
     reference_no character varying,
-    vat_rate numeric(10,2) NOT NULL,
+    vat_rate numeric(4,3) NOT NULL,
     paid_at timestamp without time zone,
     seller_id integer,
     seller_name character varying NOT NULL,
@@ -4846,4 +4846,6 @@ INSERT INTO schema_migrations (version) VALUES ('20171121233843');
 INSERT INTO schema_migrations (version) VALUES ('20171123035941');
 
 INSERT INTO schema_migrations (version) VALUES ('20171126223822');
+
+INSERT INTO schema_migrations (version) VALUES ('20171127014039');
 
