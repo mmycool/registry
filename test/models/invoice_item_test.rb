4e27ef6417c28bbeb533c902a408251b482dddd7
Artur Beljajev <artur.beljajev@internet.ee>
2018-10-14 18:08:48 +0300

    Validate fixture

--- test/models/invoice_item_test.rb
+++ test/models/invoice_item_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class InvoiceItemTest < ActiveSupport::TestCase
+  def setup
+    @invoice_item = invoice_items(:one)
+  end
+
+  def test_fixture_is_valid
+    assert @invoice_item.valid?
+  end
+
   def test_calculates_amount
     invoice_item = InvoiceItem.new(price: 5, quantity: 2)
     assert_equal 10, invoice_item.amount
