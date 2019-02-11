c4ff28a2b13043da6a6f8552708d462e1ee02bcf
Artur Beljajev <artur.beljajev@internet.ee>
2019-02-11 16:59:14 +0200

    Rename company register settings

--- config/initializers/company_register.rb
+++ config/initializers/company_register.rb
@@ -1,7 +1,7 @@
 CompanyRegister.configure do |config|
-  config.username = ENV['arireg_username']
-  config.password = ENV['arireg_password']
-  config.wsdl = ENV['arireg_wsdl']
-  config.endpoint = ENV['arireg_host']
-  config.cache_period = ENV['arireg_cache_period_days'].to_i.days
+  config.username = ENV['company_register_username']
+  config.password = ENV['company_register_password']
+  config.wsdl = ENV['company_register_wsdl']
+  config.endpoint = ENV['company_register_host']
+  config.cache_period = ENV['company_register_cache_period_days'].to_i.days
 end
