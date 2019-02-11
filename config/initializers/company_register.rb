d7c6fdcfe1b21476b38d6be87c4a9757cb693a1f
Artur Beljajev <artur.beljajev@internet.ee>
2019-02-12 01:52:10 +0200

    Update `company_register` gem

--- config/initializers/company_register.rb
+++ config/initializers/company_register.rb
@@ -3,5 +3,6 @@ CompanyRegister.configure do |config|
   config.password = ENV['company_register_password']
   config.wsdl = ENV['company_register_wsdl']
   config.endpoint = ENV['company_register_host']
+  config.cache_store = Rails.cache
   config.cache_period = ENV['company_register_cache_period_days'].to_i.days
 end
