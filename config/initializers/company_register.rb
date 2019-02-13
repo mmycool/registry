bb06d8d87d9794ac28c8dc669b3bb4cab44a944d
Artur Beljajev <artur.beljajev@internet.ee>
2019-02-14 00:11:51 +0200

    Remove `company_register_wsdl` and `company_register_host` settings

--- config/initializers/company_register.rb
+++ config/initializers/company_register.rb
@@ -1,8 +1,6 @@
 CompanyRegister.configure do |config|
   config.username = ENV['company_register_username']
   config.password = ENV['company_register_password']
-  config.wsdl = ENV['company_register_wsdl']
-  config.endpoint = ENV['company_register_host']
   config.cache_store = Rails.cache
   config.cache_period = ENV['company_register_cache_period_days'].to_i.days
 end
