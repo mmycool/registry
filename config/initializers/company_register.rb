cb693c14480a75615a32b217e899f2814c6e5c3f
Artur Beljajev <artur.beljajev@internet.ee>
2019-02-14 16:10:01 +0200

    Add `company_register_test_mode` config

--- config/initializers/company_register.rb
+++ config/initializers/company_register.rb
@@ -1,6 +1,6 @@
 CompanyRegister.configure do |config|
   config.username = ENV['company_register_username']
   config.password = ENV['company_register_password']
-  config.cache_store = Rails.cache
   config.cache_period = ENV['company_register_cache_period_days'].to_i.days
+  config.test_mode = ENV['company_register_test_mode'] == 'true'
 end
